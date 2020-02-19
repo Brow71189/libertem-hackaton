04268482d84172146d2723b3374504593983954e
Sherjeel Shabih <shabihsherjeel@gmail.com>
2020-02-19 17:42:13 +0100

    Added a hardcoded path to load "/data" from the hdf5 file

--- nionswift_plugin/nionswift_libertem/libertem_adapter.py
+++ nionswift_plugin/nionswift_libertem/libertem_adapter.py
@@ -6,10 +6,10 @@ class LiberTEMAdapter:
         self.__api = api
         self.__executor = executor
 
-    def niondata_to_libertemdata(self, niondata, ds_path):
+    def niondata_to_libertemdata(self, niondata):
         sig_dims = niondata.xdata.datum_dimension_count
         niondata = niondata._data_item
         filepath = niondata.persistent_storage.get_storage_property(niondata, "file_path")
 
-        file_parameters = {"path":filepath, "ds_path":ds_path, "sig_dims":sig_dims}
+        file_parameters = {"path":filepath, "ds_path":"data", "sig_dims":sig_dims}
         return dataset.load("hdf5", self.__executor.ensure_sync(), **file_parameters)
