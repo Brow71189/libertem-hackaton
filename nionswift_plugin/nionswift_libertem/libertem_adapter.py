4913e8cd2587b9bb4a0f28d41118e595f68b1ab0
Sherjeel Shabih <shabihsherjeel@gmail.com>
2020-02-19 17:32:26 +0100

    Added a simple adapter to return a libertem dataset from a nion data item

---
+++ nionswift_plugin/nionswift_libertem/libertem_adapter.py
@@ -0,0 +1,15 @@
+from nion.typeshed import API_1_0
+from libertem.io import dataset
+
+class LiberTEMAdapter:
+    def __init__(self, api: API_1_0.API, executor):
+        self.__api = api
+        self.__executor = executor
+
+    def niondata_to_libertemdata(self, niondata, ds_path):
+        sig_dims = niondata.xdata.datum_dimension_count
+        niondata = niondata._data_item
+        filepath = niondata.persistent_storage.get_storage_property(niondata, "file_path")
+
+        file_parameters = {"path":filepath, "ds_path":ds_path, "sig_dims":sig_dims}
+        return dataset.load("hdf5", self.__executor.ensure_sync(), **file_parameters)
