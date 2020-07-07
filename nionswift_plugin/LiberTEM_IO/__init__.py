Enable select map graphic computation UI after fixing it.

nionswift_plugin/LiberTEM_IO/__init__.py
@@ -37,7 +37,7 @@ class LiberTEMIODelegate:
         self.__file_param_dialog_closed_event = threading.Event()
         self.__file_param_dialog_closed_event.set()
         self.__show_file_param_dialog_finished_event = threading.Event()
-        
+
     def show_file_param_dialog(self, file_ext: str=None, params_callback: callable=None):
         if self.__file_param_dialog_closed_event.is_set():
             document_controller = self.__api.application.document_controllers[0]._document_controller
@@ -57,12 +57,12 @@ class LiberTEMIODelegate:
                 ui_handler.init_handler()
 
             dialog.show()
-            
+
             ui_handler.request_close = dialog.request_close
 
             self.__file_param_dialog_closed_event.clear()
         self.__show_file_param_dialog_finished_event.set()
-        
+
     def can_write_data_and_metadata(self, data_and_metadata, extension):
         return False
 
@@ -77,21 +77,21 @@ class LiberTEMIODelegate:
         executor = executor.ensure_sync()
         file_parameters = dataset.detect(stream, executor=executor)
         file_type = file_parameters.get('type', None)
-        file_parameters = file_parameters["parameters"]
+        file_parameters = file_parameters.get('parameters', dict())
         if file_type is None:
             file_type = 'raw'
             file_parameters = {'path': stream}
         file_params = dict()
         def params_callback(file_params_):
             file_params.update(file_params_)
-            
+
         self.__api.queue_task(lambda: self.show_file_param_dialog(file_type, params_callback))
         self.__show_file_param_dialog_finished_event.wait()
         self.__show_file_param_dialog_finished_event.clear()
         self.__file_param_dialog_closed_event.wait()
         file_params.pop('name', None)
         file_parameters.update(file_params)
-        
+
         ds = dataset.load(file_type, executor, **file_parameters)
         roi = np.zeros(ds.shape.nav, dtype=bool)
         roi_flat = roi.ravel()
@@ -104,7 +104,7 @@ class LiberTEMIODelegate:
 
     def write_data_item(self, data_item, file_path, extension) -> None:
         self.write_data_item_stream(data_item, file_path)
-        
+
     def write_data_item_stream(self, data_item, stream) -> None:
         self.write_data_and_metadata_stream(data_item.xdata, stream)
 

nionswift_plugin/nionswift_libertem/Map4D.py
@@ -33,24 +33,24 @@ class Map4D:
 
         def create_panel_widget(ui, document_controller):
             def select_button_clicked():
-                graphics = Facade.DataItem(self.computation._computation.source).graphics
+                graphics = []
+                for graphic in document_controller.target_display.graphics:
+                    if graphic._graphic.used_role in {'mask', 'fourier_mask'}:
+                        graphics.append(graphic)
                 if not graphics:
                     return
-                graphics_variable = self.computation._computation._get_variable('map_regions')
-                graphics_variable.disconnect_items()
-                if graphics_variable.bound_items_model is None:
-                    return
-                num_items = len(graphics_variable.bound_items_model.items)
-                for _ in range(num_items):
-                    self.computation._computation.remove_item_from_objects('map_regions', 0)
+                try:
+                    while True:
+                        self.computation._computation.remove_item_from_objects('map_regions', 0)
+                except IndexError:
+                    pass
                 for graphic in graphics:
-                    if graphic._graphic.role == 'mask':
-                        self.computation._computation.insert_item_into_objects('map_regions', 0, Symbolic.make_item(graphic._graphic, type='graphic'))
+                    self.computation._computation.insert_item_into_objects('map_regions', 0, Symbolic.make_item(graphic._graphic))
 
             column = ui.create_column_widget()
             row = ui.create_row_widget()
 
-            select_graphics_button = ui.create_push_button_widget('Update masks')
+            select_graphics_button = ui.create_push_button_widget('Select map graphic')
             row.add_spacing(10)
             row.add(select_graphics_button)
             row.add_stretch()
@@ -64,8 +64,8 @@ class Map4D:
             select_graphics_button.on_clicked = select_button_clicked
 
             return column
-        # Disable mask updating for now because it is broken
-        # self.computation._computation.create_panel_widget = create_panel_widget
+
+        self.computation._computation.create_panel_widget = create_panel_widget
 
     def get_xdata_for_results(self, result_array):
         data_descriptor = self.__api.create_data_descriptor(False, 0, 2)
@@ -250,7 +250,9 @@ class Map4DMenuItem:
                 new_metadata = copy.deepcopy(src.metadata)
                 new_display_slice = np.ravel_multi_index(current_index, target.data.shape)
                 new_metadata['libertem-io']['display_slice']['start'] = new_display_slice
-                new_xdata = self.__api.create_data_and_metadata(result_array, metadata=new_metadata)
+                new_xdata = self.__api.create_data_and_metadata(result_array, metadata=new_metadata,
+                                                                intensity_calibration=src.intensity_calibration,
+                                                                dimensional_calibrations=src.dimensional_calibrations)
                 src.set_data_and_metadata(new_xdata)
 
         if do_wait > 0:
@@ -305,18 +307,18 @@ class Map4DMenuItem:
             show_display_item(window, display_item)
             map_regions = list()
             for graphic in api_data_item.graphics:
-                if graphic._graphic.role == 'mask':
+                if graphic._graphic.used_role in {'mask', 'fourier_mask'}:
                     map_regions.append(graphic)
             computation = self.__api.library.create_computation('nion.libertem.map_4d',
                                                                 inputs={'src': api_data_item,
                                                                         'map_regions': map_regions},
                                                                 outputs={'target': map_data_item})
-            computation._computation.source = data_item
+            computation._computation.source = map_data_item._data_item
             if ds is not None:
                 computation._computation.ds = ds
 
-            map_display_item = document_controller.document_model.get_display_item_for_data_item(map_data_item)
-            document_controller.show_display_item(map_display_item)
+            #map_display_item = document_controller.document_model.get_display_item_for_data_item(map_data_item._data_item)
+            #document_controller.show_display_item(map_display_item)
             pick_graphic = map_data_item.add_point_region(0.5, 0.5)
             pick_graphic.label = 'Pick'
 